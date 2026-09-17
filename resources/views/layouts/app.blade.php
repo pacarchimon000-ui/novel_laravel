9156ba91fbc099b623c1b174a417c1d45d3dc6e1
Sebastian Botu <pacarchimon@gmail.com>
2026-09-17 23:38:32 +0700

    Fix: update layout to use Vite directive @vite instead of asset() for CSS/JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Merriweather:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
     @stack('styles')
     @stack('scripts')
 </head>
